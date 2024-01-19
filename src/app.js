feb342c5baeabb1dcd83928673e9a824e37ce51f
Petar Cholakov <petar.cholakov30@gmail.com>
2024-01-19 08:55:32 +0000

    Set up modular routing config

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ let port = 6969;
 
 let app = express();
 
+let router = require('./routes/routes');
+
+app.use(router)
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
