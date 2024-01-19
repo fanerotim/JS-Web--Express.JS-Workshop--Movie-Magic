36337ec8777e760f90271ee1851517f57cf73da3
Petar Cholakov <petar.cholakov30@gmail.com>
2024-01-19 12:17:36 +0000

    set up handlebars and path dependencies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+let handlebars = require('express-handlebars');
+let path = require('path');
+
 let port = 6969;
 
 let app = express();
+
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
 
 let router = require('./routes/routes');
 
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World. This is my first Express.JS Workshop');
+    res.render('home', {layout: false});
 })
 
 
