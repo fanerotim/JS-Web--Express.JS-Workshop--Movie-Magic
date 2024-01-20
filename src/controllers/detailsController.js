1e669fc87df6f4cb24c2056895a5592f5d714207
Petar Cholakov <petar.cholakov30@gmail.com>
2024-01-20 12:35:44 +0000

    Set up details page

--- src/controllers/detailsController.js
+++ src/controllers/detailsController.js
@@ -1,11 +1,19 @@
 let router = require('express').Router();
 let database = require('../database');
 
-router.get('/:movieId', (req, res) => {
+router.get('/details/:movieId', (req, res) => {
     let movieId = Number(req.params.movieId);
     let movieInfo = database.filter(movie => movie.id === movieId);
 
+    let ratingArr = [];
+
+    for (let i = 0; i < movieInfo[0].rating; i++) {
+        ratingArr.push(i);
+    }
+
+    movieInfo[0].rating = ratingArr;
     res.render('details', {layout: false, movieInfo});
+    
 })
 
 module.exports = router;
