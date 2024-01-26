0fd5256c32bb3ae7a049df95507ac09db225338f
Petar Cholakov <petar.cholakov30@gmail.com>
2024-01-26 19:28:08 +0000

    fixed rating information on details page

--- src/controllers/detailsController.js
+++ src/controllers/detailsController.js
@@ -6,13 +6,12 @@ router.get('/details/:movieId', async (req, res) => {
     let movieId = req.params.movieId;
     let movies = await Movies.findById(movieId).lean();
     
-    //I have to fix the rating (stars)
     let ratingArr = [];
     
     for (let i = 0; i < movies.rating; i++) {
         ratingArr.push(i);
     }
-    movies['stars'] = ratingArr;
+
     res.render('details', {layout: false, movies, ratingArr});
     
 })
