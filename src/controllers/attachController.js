6828d187fb0285ce6996ae16db3c4a449eeef454
Petar Cholakov <petar.cholakov30@gmail.com>
2024-01-27 17:59:09 +0000

    set up attach to load cast dynamically from mongoose database

--- src/controllers/attachController.js
+++ src/controllers/attachController.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
 const Movies = require('../models/Movies');
+const Cast = require('../models/Cast');
 
 router.get('/attach/cast/:castId', async (req, res) => {
     let castId = req.params.castId;
     let movie = await Movies.findById(castId).lean();
-    console.log(movie);
-    res.render('cast-attach', {layout: false, movie})
+    let cast = await Cast.find().lean();
+
+    res.render('cast-attach', {layout: false, movie, cast})
 })
 
 module.exports = router;
