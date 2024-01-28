3a9011c4f36204369cf2a224503024751656d805
Petar Cholakov <petar.cholakov30@gmail.com>
2024-01-28 12:37:35 +0000

    set up attach page to add casts to database properly

--- src/controllers/attachController.js
+++ src/controllers/attachController.js
@@ -1,7 +1,9 @@
-const router = require('express').Router();
+const express = require('express')
+const router = express.Router();
 
 const Movies = require('../models/Movies');
 const Cast = require('../models/Cast');
+const { urlencoded } = require('express');
 
 router.get('/attach/cast/:castId', async (req, res) => {
     let castId = req.params.castId;
@@ -9,6 +11,19 @@ router.get('/attach/cast/:castId', async (req, res) => {
     let cast = await Cast.find().lean();
 
     res.render('cast-attach', {layout: false, movie, cast})
+})
+
+router.post('/attach/cast/:castId', express.urlencoded({extended: false}), async (req, res) => {
+    let castId = req.params.castId;
+    const castName = req.body.cast;
+
+    let castInfo = await Cast.find({name: castName});
+    let castObjectId = castInfo[0]._id;
+    let movie = await Movies.findById(castId);
+    movie.cast.push(castObjectId);
+    movie.save();
+
+    res.redirect(`/attach/cast/${castId}`);
 })
 
 module.exports = router;
